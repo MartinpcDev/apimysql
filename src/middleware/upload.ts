6ac5225dd481ef28d75221c53dfd379107658923
martin <MartinChrisPC@hotmail.com>
2024-01-22 08:33:27 -0500

    [UPDATE] filfilter config multer

--- src/middleware/upload.ts
+++ src/middleware/upload.ts
@@ -2,12 +2,22 @@ import { Request } from "express";
 import multer from "multer";
 
 export const upload = multer({
+  fileFilter: (req: Request, file: Express.Multer.File, cb: any) => {
+    const fileTypes = /jpeg|jpg|png|gif/
+    const mimetype = fileTypes.test(file.mimetype)
+    if(mimetype) {
+      cb(null,true)
+    }else {
+      cb('Invalid image type')
+    }
+  },
+  limits: { fileSize: 5000000 },
   storage: multer.diskStorage({
-    destination: (req: Request,file:Express.Multer.File,cb:any) => {
-      cb(null,`${process.cwd()}/storage`)
+    destination: (req: Request, file: Express.Multer.File, cb: any) => {
+      cb(null, `${process.cwd()}/storage`)
     },
     filename: (req: Request, file: Express.Multer.File, cb: any) => {
       cb(null, file.originalname)
-    }
+    },
   })
 })
